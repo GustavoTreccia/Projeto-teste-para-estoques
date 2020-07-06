9928a4f10e3f768a0be78ff294d7015e2b1090ce
GustavoTreccia <g.treccia@outlook.com>
2020-07-06 20:15:49 -0300

    Product progress

--- src/main/java/com/fatec/easydepots/resources/ProductResource.java
+++ src/main/java/com/fatec/easydepots/resources/ProductResource.java
@@ -1,6 +1,5 @@
 package com.fatec.easydepots.resources;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fatec.easydepots.entities.Category;
-import com.fatec.easydepots.repositories.CategoryRepository;
+import com.fatec.easydepots.entities.Product;
+import com.fatec.easydepots.repositories.ProductRepository;
 
 // RequestMapping == Geralmente utilizada em cima dos métodos de uma classe anotada com @Controller. 
 // Serve para você colocar os endereços da sua aplicação que, quando acessados por algum cliente
@@ -21,21 +20,21 @@ import com.fatec.easydepots.repositories.CategoryRepository;
 // Você pode colocar ela sobre atributos ou sobre o seu construtor com argumentos.
 
 @RestController
-@RequestMapping(value = "/categories")
-public class CategoryResource {
+@RequestMapping(value = "/products")
+public class ProductResource {
 	
 	@Autowired
-	private CategoryRepository categoryRepository;
+	private ProductRepository categoryRepository;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = categoryRepository.findAll();
+	public ResponseEntity<List<Product>> findAll(){
+		List<Product> list = categoryRepository.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Category> findById(@PathVariable Long id){
-		Category cat = categoryRepository.findById(id);
+	public ResponseEntity<Product> findById(@PathVariable Long id){
+		Product cat = categoryRepository.findById(id);
 		return ResponseEntity.ok().body(cat);
 	}
 	
